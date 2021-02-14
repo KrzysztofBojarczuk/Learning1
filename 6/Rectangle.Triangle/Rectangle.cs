7ca4cd648ebc9899454408fef844f5c7351ad356
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-14 14:40:59 +0100

    Create Rectangle.cs

---
+++ 6/Rectangle.Triangle/Rectangle.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp30
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+                
+        }
+    }
+}
